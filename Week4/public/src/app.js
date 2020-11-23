13f3f965466f5b18f64a0d733870986898c72721
dsm <dmccormick@regis.edu>
2020-11-22 19:59:32 -0700

    week 4 first commit

--- Week4/public/src/app.js
+++ Week4/public/src/app.js
@@ -1,23 +1,32 @@
-var object = {
-    name: 'Bob',
-    age: 50
+const doLogin = function(e) {
+    e.preventDefault();
+    const username = document.getElementById('username').nodeValue;
+    const password = document.getElementById('password').nodeValue;
+
+    login({
+        username: username,
+        password: password
+    }).then(function(res){
+        window.location.href = 'home.html';
+    });
+};
+
+const doRegister = function(e) {
+    e.preventDefault();
+    const username = document.getElementById('username').nodeValue;
+    const email = document.getElementById('email').nodeValue;
+    const password = document.getElementById('password').nodeValue;
+
+    register({
+        username: username,
+        email, email,
+        password, password
+    }).then(function(res){
+        window.location.href = 'home.html'
+    });
+    
+};
+
+const doLogout = function(e) {
+    e.preventDefault();
 }
-
-class Person {
-    firstname;
-    lastname;
-    age;
-
-    constructor(firstname, lastname, age) {
-        this.firstname = firstname;
-        this.lastname = lastname;
-        this.age = age;
-    }
-
-    getFullName() {
-        return this.firstname + " " + this.lastname;
-    }
-
-}
-
-console.log(new Person('Bob', 'Jones', 50).getFullName());
