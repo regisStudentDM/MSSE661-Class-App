1ca2115383e10b3688953dcb8a4b662ac69e2f50
Daniel McCormick <dmccormick@regis.edu>
2020-12-04 18:49:48 -0700

    replacing tasks with parts

--- Week6/public/src/route-guard.js
+++ Week6/public/src/route-guard.js
@@ -2,7 +2,7 @@
   const isAuth = getStorage('isAuth');
   if (!isAuth) {
     logout();
-    alert('Log in to view your tasks.');
+    alert('Log in to view your parts.');
     window.location.href = '/login.html';
   }
 })();
